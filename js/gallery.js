d28e7b440cb8c87d8ec5af5694d2c1003f3bb9e1
mayank-wadhwa-git <68892762+mayank-wadhwa-git@users.noreply.github.com>
2021-08-24 18:07:27 +0530

    Updated Pages

--- js/gallery.js
+++ js/gallery.js
@@ -3,8 +3,8 @@ function createGallery(foldername, n){
     for (let index = 1; index <= n; index++) {
         
         htmlText += ` <div class="col-md-4">
-        <a href="img/gallery/${foldername}/image (${index}).jpg" class="img-pop-up">
-          <div class="single-gallery-image" style="background: url('img/gallery/${foldername}/image (${index}).jpg');"></div>
+        <a href="gallery/${foldername}/image (${index}).jpg" class="img-pop-up">
+          <div class="single-gallery-image" style="background: url('gallery/${foldername}/image (${index}).jpg');"></div>
         </a>
       </div>`
     }
@@ -13,8 +13,8 @@ function createGallery(foldername, n){
 }
 
 document.getElementById('covidGallery').innerHTML = createGallery('covid', 18)
-document.getElementById('drawingcompGallery').innerHTML = createGallery('drawingcomp', 18)
 document.getElementById('educationGallery').innerHTML = createGallery('education', 18)
 document.getElementById('holiGallery').innerHTML = createGallery('holi', 18)
 document.getElementById('lohriGallery').innerHTML = createGallery('lohri', 18)
 document.getElementById('sanitationGallery').innerHTML = createGallery('sanitation', 18)
+document.getElementById('drawingcompGallery').innerHTML = createGallery('drawing', 18)
