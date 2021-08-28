b769c671bc7315d49e314afeb59728eba5cbea4d
mayank-wadhwa-git <68892762+mayank-wadhwa-git@users.noreply.github.com>
2021-08-28 18:40:41 +0530

    Added Validations to joinUs

--- js/joinUs.js
+++ js/joinUs.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     (function ($) {
         "use strict"; jQuery.validator.addMethod('answercheck', function (value, element) { return this.optional(element) || /^\bcat\b$/.test(value) }, "type the correct answer -_-"); $(function () {
-            $('#contactForm').validate({
+            $('#joinUsForm').validate({
                 rules: { name: { required: true, minlength: 2 }, subject: { required: true, minlength: 4 }, number: { required: true, minlength: 5 }, email: { required: true, email: true }, message: { required: true, minlength: 20 } }, messages: { name: { required: "come on, you have a name, don't you?", minlength: "your name must consist of at least 2 characters" }, subject: { required: "come on, you have a subject, don't you?", minlength: "your subject must consist of at least 4 characters" }, number: { required: "come on, you have a number, don't you?", minlength: "your Number must consist of at least 5 characters" }, email: { required: "no email, no message" }, message: { required: "um...yea, you have to write something to send this form.", minlength: "thats all? really?" } }, submitHandler: function (form) {
                     $(form).ajaxSubmit({
-                        type: "POST", data: $(form).serialize(), url: /*"contact_process.php"*/"https://api.apispreadsheets.com/data/16899/", success: function () {
-                            $('#contactForm :input').attr('disabled', 'disabled'); $('#contactForm').fadeTo("slow", 1, function () {
+                        type: "POST", data: $(form).serialize(), url: /*"contact_process.php"*/"https://api.apispreadsheets.com/data/16900/", success: function () {
+                            $('#joinUsForm :input').attr('disabled', 'disabled'); $('#joinUsForm').fadeTo("slow", 1, function () {
                                 $(this).find(':input').attr('disabled', 'disabled'); $(this).find('label').css('cursor', 'default'); $('#success').fadeIn()
                                 $('.modal').modal('hide'); $('#success').modal('show');
                             })
                             window.location.replace("redirect.html")
                         }, error: function () {
-                            $('#contactForm').fadeTo("slow", 1, function () {
+                            $('#joinUsForm').fadeTo("slow", 1, function () {
                                 $('#error').fadeIn()
                                 $('.modal').modal('hide'); $('#error').modal('show');
                             })
